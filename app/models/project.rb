c01747dc51effe24049cb7c9be86cea3717f6d2b
delius <pawel@fibrotrust.org>
2014-08-22 15:26:24 +0100

    added validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ActiveRecord::Base
 	belongs_to :company
 	has_many :works
 	has_many :users, :through => :works
+
+	validates :name, presence: true
+
+	scope :lowdefaultrate, => { where ("default_rate < 100") }
 end
+
