5ef41d9e8191deb2512141b80daf6b4321e74d35
delius <pawel@fibrotrust.org>
2014-08-22 20:38:04 +0100

    updated models and views works and projects

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,12 +6,11 @@ class Work < ActiveRecord::Base
 	validates :project, presence: true
 	validates :user, presence: true
 	validates :datetimeperformed, presence: true
-	validates :hours, numericality: { only_integer: true, greater_than: 0
-										less_than_or_equal_to: 8 }
+	validates :hours, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 8 }
 	
 
-	scope :fullday, => { where ("hours >= 8")}
-	scope :recent, => {where ( "datetimeperformed > '#{Time.now - 7.days}'")}
+	scope :fullday, -> { where("hours >= 8") }
+	scope :recent, -> {where ( "datetimeperformed > '#{Time.now - 7.days}'") }
 
 	def date_is_in_past
 		if datetimeperformed.present? && datetimeperformed > Time.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Timetracker::Application.routes.draw do
 
 resources :companies
-
+resources :projects
+resources :works
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
