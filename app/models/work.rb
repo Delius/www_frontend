e591045d98d8a84c7249f3493de611e6ddc0a207
delius <pawel@fibrotrust.org>
2014-08-22 16:38:04 +0100

    added work validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,8 +1,14 @@
 class Work < ActiveRecord::Base
+	
 	belongs_to :project
 	belongs_to :user
 
-	validates :hours, numericality: { only_integer: true, greater_than: 0 }
+	validates :project, presence: true
+	validates :user, presence: true
+	validates :datetimeperformed, presence: true
+	validates :hours, numericality: { only_integer: true, greater_than: 0
+										less_than_or_equal_to: 8 }
+	
 
 	scope :fullday, => { where ("hours >= 8")}
 	scope :recent, => {where ( "datetimeperformed > '#{Time.now - 7.days}'")}
