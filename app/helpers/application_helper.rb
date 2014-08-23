824af92a7b2f438500b0837bd3f3ca74a3ca4807
delius <pawel@fibrotrust.org>
2014-08-23 11:48:40 +0100

    added application helper nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def display_nav_item(displaytext, controllername, linkurl)
+		rav("<li#{controllername==controllername ? 'class="acitve"' : ''}>#{link_to displaytext, linkurl}</li>")
+	end
 end
