21a9db503cce21364024f72d2884c0ff8c7ac527
delius <pawel@fibrotrust.org>
2014-08-23 11:56:47 +0100

    modified view according to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,26 @@
 module ApplicationHelper
+
+def nav_items
+
+	{
+		displaytext: 'Companies',
+		controllername: 'companies',
+		linkurl: companies-path
+	},
+	{
+		displaytext: 'Projects',
+		controllername: 'projects',
+		linkurl: projects_path
+	},
+	{
+		displaytext: 'Works',
+		controllername: 'works',
+		linkurl: works_path
+	}
+
+
+
+
 	def display_nav_item(displaytext, controllername, linkurl)
 		rav("<li#{controllername==controllername ? 'class="acitve"' : ''}>#{link_to displaytext, linkurl}</li>")
 	end
