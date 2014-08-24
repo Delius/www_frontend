6d6f4af3c9082a81664f882baf853cfebd63e99e
delius <pawel@fibrotrust.org>
2014-08-24 12:19:56 +0100

    json xml format

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,15 @@
 class CompaniesController < ApplicationController
 	def index
-	@companies = Company.all
+		@companies = Company.all
 	end
 
 	def show
 		@company = Company.find(params[:id])
+		respond_to do |format|
+			format.html 
+			format.xml {render xml: @company}
+			format.json {render json: @company}
+		end
+
+
 	end
-
-
-end
