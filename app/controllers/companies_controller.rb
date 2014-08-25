64ffcd5bfd97f9d427fd812d733b5b214a0db4d0
delius <pawel@fibrotrust.org>
2014-08-25 21:03:16 +0100

    added create new company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -16,4 +16,10 @@ class CompaniesController < ApplicationController
 	def new
 		@company = Company.new
 	end
+
+	def create
+		@company = Company.new(params[:company].permit(:name))
+		@company.save
+		redirect_to @company
+	end
 end
