3b6decf0a2ac75058ca3e5929ca59020996b435c
delius <pawel@fibrotrust.org>
2014-08-30 16:43:17 +0100

    added create update new edit show to examples

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,6 +1,46 @@
 class ExamplesController < ApplicationController
 
-def index
-end
+
+	def index
+		@examples = Example.all
+		respond_to do |format|
+			format.html
+		end
+	end
+
+	def show
+
+			@example = Example.find(params[:id])
+		end
+		
+
+	def new
+		@example = Example.new
+	end
+
+	def create
+		@example = Example.new(params[:example].permit(:name, :technology_id)
+		if @example.save
+			flash[:notice] = 'Example Created'
+			redirect_to @example
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@example = Wxample.find(params[:id])
+	end
+
+	def update
+		@example = Example.find(params[:id])
+
+		if @example.update(params[:example].permit(:name, :technology_id))
+			flash[:notice] = 'Example Updated'
+			redirect_to @example
+		else
+			render 'edit'
+		end
+	end
 
 end
