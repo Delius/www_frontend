82c715c88c9599d799ad58592ad039cd11462131
delius <pawel@fibrotrust.org>
2014-09-02 17:28:08 +0100

    add tags methods

--- app/models/example.rb
+++ app/models/example.rb
@@ -2,6 +2,29 @@ class Example < ActiveRecord::Base
 	belongs_to :technology
 	belongs_to :user
 
+	has_many :taggings
+	has_many :tags, through: :taggings
+
+
+	def self.tagged_with(name)
+		Tag.find_by_name!(name).examples
+	end
+
+	def self.tag_counts
+		Tag.select("tags.name, count(taggings.tag_id) as count").
+		joins(:taggings).group("taggings.tag_id")
+	end
+
+	def all_tags
+		tags.map(&:name).join(", ")
+	end
+
+	def all_tags=(names)
+		self.tags = names.split(",").map do |n|
+			Tag.where(name: n.strip).first_or_create!
+		end
+	end
+
 	def to_s
 		"#{name}"
 	end
